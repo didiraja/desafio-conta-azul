4f63b1c59a6bae95d0c118865db1bd44c5383270
didiraja <didiraja@hotmail.com>
2022-06-30 22:10:09 -0300

    refactor: 💡 basic grid and responsive

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { useState, useEffect } from 'react';
 import "./App.sass";
 import { getLocationData } from './requests';
 import CityBox from "./components/CityBox";
-import AppStyle from "./App.module.sass";
+import styles from "./App.module.sass";
 import OWLogo from './assets/logo.svg';
+import { urubici, nuuk, nairobi } from './response';
 
 const timeNow = new Date().toLocaleString("en-US", {
   hour: "numeric",
@@ -15,14 +16,14 @@ const timeNow = new Date().toLocaleString("en-US", {
 
 const places = [
   {
-    name:'nuuk',
-    lat: 64.1835,
-    lon: -51.7216,
-  },
-  {
     name:'urubici',
     lat: -28.0274,
     lon: -49.6125,
+  },
+  {
+    name:'nuuk',
+    lat: 64.1835,
+    lon: -51.7216,
   },
   {
     name:'nairobi',
@@ -49,19 +50,17 @@ function App() {
   }, [], () => setLocations([]));
 
   return (
-      <div className={`App ${AppStyle.app}`}>
-        <header className={AppStyle.header}>
+      <>
+        <header className={styles.header}>
           <img src={OWLogo} alt="OpenWeather - Logo" />
         </header>
 
-        <main>
-          {
-            locations.map((item, index) => {
-              return <CityBox key={index} {...item} {...updatedAt} />;
-            })
-          }
+        <main className={styles.container}>
+          <CityBox {...urubici} style={{gridArea: 'A'}} {...updatedAt} />
+          <CityBox {...nuuk} style={{gridArea: 'B'}} {...updatedAt} />
+          <CityBox {...nairobi} style={{gridArea: 'C'}} {...updatedAt} />
         </main>
-      </div>
+      </>
   );
 }
 
--- src/components/CityBox.js
+++ src/components/CityBox.js
@@ -1,6 +1,6 @@
 import styles from "./CityBox.module.sass";
 
-function CityBox({ name, sys, main, updatedAt }) {
+function CityBox({ style, name, sys, main, updatedAt }) {
 
   function tempColor() {
     if (main && main.temp) {
@@ -12,16 +12,35 @@ function CityBox({ name, sys, main, updatedAt }) {
   }
 
   return (
-    <div className="city-box">
-      <ul>
-        <li>
-          {name}, {sys.country}
-        </li>
-        <li className={tempColor()}>Temperatura {main.temp}°</li>
-        <li>Umidade em {main.humidity}%</li>
-        <li>Pressão em {main.pressure}hPa</li>
-        <li>Updated at {updatedAt}</li>
-      </ul>
+    <div className={styles.cityBox} style={{...style}}>
+      <div className={styles.cityNameBox}>
+        <p className={styles.cityName}>{name}, {sys.country}</p>
+      </div>
+
+      <div className={styles.cityTempBox}>
+        <p className={`${styles.cityTemp} ${tempColor()}`}>
+          {main.temp.toFixed()}°
+        </p>
+      </div>
+
+        <div className={styles.cityCardBottom}>
+          { name === 'Urubici' &&
+            <div className={styles.cityCardInfo}>
+              <div>
+                <p className={styles.title}>Humidity</p>
+                <p className={styles.value}>{main.humidity}%</p>
+              </div>
+              <div>
+                <p className={styles.title}>Pressure</p>
+                <p className={styles.value}>{main.pressure}hPa</p>
+              </div>
+            </div>
+          }
+
+        <div className={styles.cityCardUpdated}>
+          <div>Updated at {updatedAt}</div>
+        </div>
+      </div>
     </div>
   );
 }
